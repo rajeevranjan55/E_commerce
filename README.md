f10b7408e78a833e7f15e61073e23068a98f1a03
rajeevranjan55 <107798733+rajeevranjan55@users.noreply.github.com>
2023-05-28 13:02:10 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ecommerce-API
+Ecommerce API project for coding ninjas assignment
+
+
+To use the API simply clone the repo in your local machine and run
+npm i
+then run
+npm start
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const productRouter = require("./routes/products");
+// const port=5005;
+
+// connecting to database
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.DB_CONNECTION_URL).then(() => console.log("DBConnection is Successful")).catch((err) => { console.log(err); });
+
+// connecting to server
+app.listen(process.env.PORT, () => {
+    console.log("Backend Server is running on port 5005");
+});
+
+// middlewares
+app.use(bodyParser.json());
+app.use("/products", productRouter);
